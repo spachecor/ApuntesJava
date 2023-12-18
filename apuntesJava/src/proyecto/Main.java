15f4bb58eebe5ed40d434988afda79f3ce8f5a50
Selene <spacrod308@g.educaand.es>
2023-12-18 21:48:15 +0100

    2º parte pruebas proyecto

--- apuntesJava/src/proyecto/Main.java
+++ apuntesJava/src/proyecto/Main.java
@@ -1,10 +1,23 @@
 package proyecto;
 
+import java.util.Scanner;
+
+/**
+ * Entrada del programa. Se solicita al usuario quién es y qué desea hacer, y según lo que escoja se le 
+ * redirige en una dirección u otra
+ * @author selene
+ * @version 1.0
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int rol, opcion;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Es usted: \n1- Administrador \n2- Usuario");
+		rol = sc.nextInt();
+		System.out.println("Desea usted acceder a: \n1- Zona animal \n2- Zona Tienda \n3- Zona donaciones"
+				+ " \n4- Salir");
+		opcion = sc.nextInt();
 	}
 
 }
--- apuntesJava/src/proyecto/Menu.java
+++ apuntesJava/src/proyecto/Menu.java
@@ -1,5 +1,9 @@
 package proyecto;
 
 public class Menu {
-
+	private final int ANIMAL=1, PRODUCTO=2, DONACION=3, SALIR=4, ADMINISTRADOR=1, USUARIO=2;
+	
+	public Menu(int rol, int opcion){
+		
+	}
 }
